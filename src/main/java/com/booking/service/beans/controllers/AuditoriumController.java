9d203f7e6cd58a8ab5eb4b1bf06c4f5849fd23ad
atsyganenko <anastasiia.tsyganenko@gmail.com>
2017-10-26 21:30:41 +0200

    Spring mvc - Auditorium view and Controller

---
+++ src/main/java/com/booking/service/beans/controllers/AuditoriumController.java
@@ -0,0 +1,61 @@
+package com.booking.service.beans.controllers;
+
+import com.booking.service.beans.models.Auditorium;
+import com.booking.service.beans.services.AuditoriumService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by Anastasiia Tsyganenko
+ * on 10/26/2017.
+ */
+
+@Controller
+@RequestMapping("auditoriums")
+public class AuditoriumController {
+
+    private final AuditoriumService auditoriumService;
+
+    public AuditoriumController(AuditoriumService auditoriumService) {
+        this.auditoriumService = auditoriumService;
+    }
+
+
+    @RequestMapping(value = "all", method = RequestMethod.GET)
+    public String getAllAuditoriums(Map<String, List<Auditorium>> model) {
+        model.put("auditoriums", auditoriumService.getAuditoriums());
+        return "auditoriumsTable";
+    }
+
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    public String getAuditoriumByID(@PathVariable String name, Map<String, List<Auditorium>> model) {
+        model.put("auditoriums", Collections.singletonList(auditoriumService.getByName(name)));
+        return "auditoriumsTable";
+    }
+
+    @RequestMapping(value = "{auditoriumName}/seats", method = RequestMethod.GET)
+    @ResponseBody
+    public Integer getSeatsNumber(@PathVariable String auditoriumName, HttpServletResponse response) {
+        response.setContentType("text/plain");
+        return auditoriumService.getSeatsNumber(auditoriumName);
+    }
+
+    @RequestMapping(value = "{auditoriumName}/seats/vip", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Integer> getVipSeats(@PathVariable String auditoriumName, HttpServletResponse response) {
+        response.setContentType("text/plain");
+        return auditoriumService.getVipSeats(auditoriumName);
+    }
+
+}
+
+
+
---
+++ src/main/resources/templates/auditoriumsTable.ftl
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<div>
+    <table>
+        <caption>Auditoriums</caption>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Seats Number</th>
+            <th>Vip Seats</th>
+        </tr>
+    <#list auditoriums as auditorium>
+        <tr>
+            <td>${auditorium.name}</td>
+            <td>${auditorium.seatsNumber}</td>
+            <td>${auditorium.vipSeats}</td>
+        </tr>
+    </#list>
+    </table>
+</div>
+</body>
+</html>
