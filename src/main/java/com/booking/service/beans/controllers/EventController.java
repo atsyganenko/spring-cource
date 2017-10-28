310e4bf4cd30bc1b2dd25d321079303b64b98df2
atsyganenko <anastasiia.tsyganenko@gmail.com>
2017-10-28 16:12:53 +0200

    Spring mvc - events controller

--- src/main/java/com/booking/service/beans/controllers/EventController.java
+++ src/main/java/com/booking/service/beans/controllers/EventController.java
@@ -3,7 +3,6 @@ package com.booking.service.beans.controllers;
 import com.booking.service.beans.models.Auditorium;
 import com.booking.service.beans.models.Event;
 import com.booking.service.beans.models.Rate;
-import com.booking.service.beans.models.User;
 import com.booking.service.beans.services.EventService;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,7 +10,9 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -30,6 +31,7 @@ import java.util.Map;
 public class EventController {
 
     private final EventService eventService;
+
     public EventController(EventService eventService) {
         this.eventService = eventService;
     }
@@ -46,7 +48,7 @@ public class EventController {
         mapper.registerModule(new JavaTimeModule());
         Event event = new Event(1, "Test event", Rate.HIGH, 124.0, java.time.LocalDateTime.of(2016, 2, 6, 14, 45, 0),
                 new Auditorium(1, "Test auditorium", 15, Arrays.asList(1, 2, 3, 4, 5)));
-        List<Event> events  = mapper.readValue(files[0].getBytes(), new TypeReference<List<Event>>() {
+        List<Event> events = mapper.readValue(files[0].getBytes(), new TypeReference<List<Event>>() {
         });
         events.forEach(eventService::create);
         return new ResponseEntity(HttpStatus.OK);
@@ -58,27 +60,17 @@ public class EventController {
         return "eventsTable";
     }
 
-    @RequestMapping("remove")
-    ResponseEntity<String> removeEvent(@ModelAttribute("event") Event event) {
-        eventService.remove(event);
-        return new ResponseEntity<>("Event removed.", HttpStatus.OK);
+    @RequestMapping("get")
+    String getEvents(@RequestParam String name, Map<String, List<Event>> model) {
+        model.put("events", eventService.getByName(name));
+        return "eventsTable";
+    }
+
+    @RequestMapping("upcoming")
+    String getUpcomingEvents(Map<String, List<Event>> model) {
+        model.put("events", eventService.getNextEvents(LocalDateTime.now()));
+        return "eventsTable";
     }
 
 }
 
-
-  /*  public Event create(Event event);
-
-    public void remove(Event event);
-
-    Event getEvent(String name, Auditorium auditorium, LocalDateTime dateTime);
-
-    public List<Event> getByName(String name);
-
-    public List<Event> getAll();
-
-    public List<Event> getForDateRange(LocalDateTime from, LocalDateTime to);
-
-    public List<Event> getNextEvents(LocalDateTime to);
-
-    public Event assignAuditorium(Event event, Auditorium auditorium, LocalDateTime date);*/
