eb0302e0fb2d6c465e2f2c30f347d5772e12cfb8
atsyganenko <anastasiia.tsyganenko@gmail.com>
2017-10-29 13:51:06 +0100

    Spring mvc - events pdf view added

--- src/main/java/com/booking/service/beans/configuration/AppConfiguration.java
+++ src/main/java/com/booking/service/beans/configuration/AppConfiguration.java
@@ -3,8 +3,6 @@ package com.booking.service.beans.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.http.MediaType;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
