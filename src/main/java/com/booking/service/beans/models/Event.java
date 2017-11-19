9a93e70cc1feeb05ff3aef2003fc3b1d402ec728
atsyganenko <anastasiia.tsyganenko@gmail.com>
2017-11-19 23:41:03 +0100

    generate pojo request-response

--- src/main/java/com/booking/service/beans/models/Event.java
+++ src/main/java/com/booking/service/beans/models/Event.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 
 @XmlRootElement(name = "event")
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(namespace = "http://booking.com", propOrder = {"id", "name", "rate", "basePrice", "dateTime", "auditorium"})
+@XmlType(namespace = "http://ws/booking/models", propOrder = {"id", "name", "rate", "basePrice", "dateTime", "auditorium"})
 public class Event {
     @XmlAttribute(required = true)
     private long id;
--- src/main/java/com/booking/service/beans/models/Rate.java
+++ src/main/java/com/booking/service/beans/models/Rate.java
@@ -2,7 +2,7 @@ package com.booking.service.beans.models;
 
 import javax.xml.bind.annotation.XmlType;
 
-@XmlType(namespace = "http://booking.com")
+@XmlType(namespace = "http://ws/booking/models")
 public enum Rate {
     HIGH, MID, LOW
 }
