8ce0307b434406b468c4eb7aa12721bddf0698a3
atsyganenko <anastasiia.tsyganenko@gmail.com>
2017-11-20 00:01:00 +0100

    namespace rename

--- src/main/java/com/booking/service/beans/configuration/WebServiceConfiguration.java
+++ src/main/java/com/booking/service/beans/configuration/WebServiceConfiguration.java
@@ -3,15 +3,22 @@ package com.booking.service.beans.configuration;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
 /**
  * Created by Anastasiia Tsyganenko
  * on 11/19/2017.
  */
+
+@EnableWs
+@Configuration
 public class WebServiceConfiguration extends WsConfigurerAdapter{
 
     @Bean
@@ -19,7 +26,7 @@ public class WebServiceConfiguration extends WsConfigurerAdapter{
         MessageDispatcherServlet servlet = new MessageDispatcherServlet();
         servlet.setApplicationContext(applicationContext);
         servlet.setTransformWsdlLocations(true);
-        return new ServletRegistrationBean(servlet, "ws/*");
+        return new ServletRegistrationBean(servlet, "/ws/*");
     }
 
     @Bean(name = "booking")
@@ -32,5 +39,9 @@ public class WebServiceConfiguration extends WsConfigurerAdapter{
         return wsdl11Definition;
     }
 
+    @Bean
+    public XsdSchema bookingSchema() {
+        return new SimpleXsdSchema(new ClassPathResource("xsd/booking_ws.xsd"));
+    }
 
 }
