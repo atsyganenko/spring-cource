2b64c90ced97c3855312fd97f028b456bd42772e
atsyganenko <anastasiia.tsyganenko@gmail.com>
2017-11-22 01:03:04 +0100

    PdfHttpMessageConverter implementation

--- src/main/java/com/booking/service/beans/helpers/PdfHttpMessageConverter.java
+++ src/main/java/com/booking/service/beans/helpers/PdfHttpMessageConverter.java
@@ -1,46 +1,86 @@
 package com.booking.service.beans.helpers;
 
 import com.booking.service.beans.models.Ticket;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
 import org.springframework.http.HttpInputMessage;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.AbstractHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.Collections;
+import java.io.OutputStream;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /**
  * Created by Anastasiia Tsyganenko
  * on 11/21/2017.
  */
-public class PdfHttpMessageConverter implements HttpMessageConverter<Ticket> {
+public class PdfHttpMessageConverter extends AbstractHttpMessageConverter<List<Object>> {
 
+    public PdfHttpMessageConverter() {
+        super(MediaType.APPLICATION_PDF);
+    }
 
     @Override
-    public boolean canRead(Class<?> aClass, MediaType mediaType) {
+    protected boolean supports(Class<?> aClass) {
+        return List.class.isAssignableFrom(aClass);
+    }
+
+    @Override
+    public boolean canRead(Class<?> clazz, MediaType mediaType) {
         return false;
     }
 
     @Override
-    public boolean canWrite(Class<?> aClass, MediaType mediaType) {
-        return true;
-    }
-
-    @Override
-    public List<MediaType> getSupportedMediaTypes() {
-        return Collections.singletonList(MediaType.APPLICATION_PDF);
-    }
-
-    @Override
-    public Ticket read(Class<? extends Ticket> aClass, HttpInputMessage httpInputMessage) throws IOException, HttpMessageNotReadableException {
+    protected List<Object> readInternal(Class<? extends List<Object>> aClass, HttpInputMessage httpInputMessage) throws IOException, HttpMessageNotReadableException {
         return null;
     }
 
     @Override
-    public void write(Ticket ticket, MediaType mediaType, HttpOutputMessage httpOutputMessage) throws IOException, HttpMessageNotWritableException {
-        httpOutputMessage.getBody().write("hello".getBytes());
+    protected void writeInternal(List<Object> list, HttpOutputMessage httpOutputMessage) throws IOException, HttpMessageNotWritableException {
+
+
+        ByteArrayOutputStream tempOutputStream = new ByteArrayOutputStream(4096);
+        Document document = new Document(PageSize.A4);
+        try {
+            PdfWriter writer = PdfWriter.getInstance(document, tempOutputStream);
+            writer.setViewerPreferences(2053);
+            document.open();
+
+            PdfPTable table = new PdfPTable(6);
+            table.addCell("ID");
+            table.addCell("Event");
+            table.addCell("Date");
+            table.addCell("Seats");
+            table.addCell("User");
+            table.addCell("Price");
+
+            list.forEach(item -> {
+                if (item instanceof Ticket) {
+                    table.addCell(String.valueOf(((Ticket) item).getId()));
+                    table.addCell(((Ticket) item).getEvent().getName());
+                    table.addCell(((Ticket) item).getDateTime().format(DateTimeFormatter.BASIC_ISO_DATE));
+                    table.addCell(((Ticket) item).getSeats());
+                    table.addCell(((Ticket) item).getUser().getName());
+                    table.addCell(String.valueOf(((Ticket) item).getPrice()));
+                }
+            });
+            document.add(table);
+            document.close();
+
+            OutputStream out = httpOutputMessage.getBody();
+            tempOutputStream.writeTo(out);
+            out.flush();
+        } catch (DocumentException e) {
+            throw new HttpMessageNotWritableException(e.getMessage());
+        }
     }
 }
